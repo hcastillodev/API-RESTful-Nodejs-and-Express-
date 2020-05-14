c28a23f6b2d53ef5451a5606fd5187fbcb43d924
Heraldo Castillo <dev.hcastillo@gmail.com>
2020-05-13 21:01:04 -0500

    Added the URL from Mlab when environment is production

--- config.js
+++ config.js
@@ -3,4 +3,9 @@ process.env.PORT = process.env.PORT || 3000 // to know port or assign port
 
 if(process.env.NODE_ENV === 'dev'){
     process.env.URL_DB = 'mongodb://localhost:27017/marathonspanama'
+}else{
+    //To use with cluster from atlas "Remember whitelist your IP ADDRESS or let allow from anywhere"
+    // devHcastillo       -> username for test
+    // Loveprogramming123 -> password for test
+    process.env.URL_DB = 'mongodb+srv://devHcastillo:Loveprogramming123@marathonspanama-tmm1v.mongodb.net/test?retryWrites=true&w=majority'
 }
